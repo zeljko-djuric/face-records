5cd1d9ea7a72a2007db383f33ef6c0e1858de70c
Zeljko Djuric <zeljko94@gmail.com>
2019-12-12 00:34:43 +0100

    Added face matching iterating through all images in directory

--- face-records.py
+++ face-records.py
@@ -1,14 +1,17 @@
 import face_recognition
+import os
 
-known_image = face_recognition.load_image_file("./img/known/mark.jpg")
+directory = "./img/known"
+
 unknown_image = face_recognition.load_image_file("./img/unknown/someone.jpg")
-
-known_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-results = face_recognition.compare_faces([known_encoding], unknown_encoding)
-
-if results[0]:
-    print("We have matching!")
-else:
-    print("We don't have matching!")
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg"):
+        known_image = face_recognition.load_image_file(
+            "./img/known/" + filename)
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        result = face_recognition.compare_faces(
+            [known_encoding], unknown_encoding)
+        if result[0]:
+            print("We have a matching!")
