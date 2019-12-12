c4cc429ff3981f3942a8fa4e057104386319769d
Zeljko Djuric <zeljko94@gmail.com>
2019-12-12 16:19:47 +0100

    AImplmented feature for adding information for unknown persons

--- face-records.py
+++ face-records.py
@@ -1,10 +1,11 @@
 import face_recognition
 import os
 import re
+import sys
 
 directory = "./img/known"
 
-unknown_image = face_recognition.load_image_file("./img/unknown/someone.jpg")
+unknown_image = face_recognition.load_image_file("./img/unknown/margot.jpg")
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 for filename in os.listdir(directory):
@@ -18,3 +19,20 @@ for filename in os.listdir(directory):
             f = open("./img/known/" + re.split(r"\.", filename)[0]+".txt", "r")
             print(f.read())
             f.close()
+            sys.exit(0)
+        else:
+            print("No data for this person!")
+            answer = input(
+                "Do you want to add information for unknown person? yes/no\n")
+            if answer == "yes":
+                f = open("new.txt", "a")
+                name = input("Full name: ")
+                f.write(name+"\n")
+                title = input("Title: ")
+                f.write(title+"\n")
+                net_worth = input("Net worth: ")
+                f.write(net_worth+"\n")
+                f.close()
+            else:
+                print("End")
+            sys.exit(0)
