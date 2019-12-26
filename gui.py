b8c229d2678cebf5618b442fa7028760ed9ae6f2
Zeljko Djuric <zeljko94@gmail.com>
2019-12-26 18:11:18 +0100

    Implemented register new person feature

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ import sys
 import re
 from PIL import Image
 import PIL
+from tkinter import messagebox
 
 window = Tk()
 window.title("Face Records App")
@@ -35,10 +36,57 @@ def OpenFile():
         if result[0]:
             print("We have a matching!\n\n")
             f = open(data+"/"+re.split(r"\.", filename)[0]+".txt", "r")
-            print(f.read())
+            info = f.read()
+            print(info)
+            messagebox.showinfo("We have a matching!", info)
             f.close()
             sys.exit(0)
-    print("NO MATCH!\n")
+    MsgBox = messagebox.askquestion(
+        'Unknown person', 'Do you want to register this person?', icon='warning')
+    if MsgBox == 'yes':
+        register_new_person()
+
+    else:
+        messagebox.showinfo(
+            'END', 'END')
+
+
+def register_new_person():
+    window1 = Tk()
+    window1.title("New window for data update")
+    window1.geometry('400x450')
+    Label(window1, text="First Name: ").grid(row=0)
+    Label(window1, text="Last Name: ").grid(row=2)
+    Label(window1, text="Title: ").grid(row=4)
+    Label(window1, text="Email: ").grid(row=6)
+    Label(window1, text="Phone: ").grid(row=8)
+    Label(window1, text="Adress: ").grid(row=10)
+    Label(window1, text="Note: ").grid(row=12)
+    e1 = Entry(window1)
+    e1.grid(row=1)
+    e2 = Entry(window1)
+    e2.grid(row=3)
+    e3 = Entry(window1)
+    e3.grid(row=5)
+    e4 = Entry(window1)
+    e4.grid(row=7)
+    e5 = Entry(window1)
+    e5.grid(row=9)
+    e6 = Entry(window1)
+    e6.grid(row=11)
+    e7 = Entry(window1)
+    e7.grid(row=13)
+
+    def profit_calculator():
+        print(e1.get())
+        print(e2.get())
+        print(e3.get())
+        print(e4.get())
+        print(e5.get())
+        print(e6.get())
+        print(e7.get())
+    button_calc = Button(window1, text="Calculate", command=profit_calculator)
+    button_calc.grid(row=14)
 
 
 btn = Button(window, text="Upload photo", command=OpenFile)
